c82a4805dd72bc49c67c33e8b52c5825d1214e30
boburjon <128900137+jumayevv@users.noreply.github.com>
2023-12-28 16:43:52 +0500

    third commit

--- src/main/java/uchinchi_oy/poligon_2.java
+++ src/main/java/uchinchi_oy/poligon_2.java
@@ -4,6 +4,6 @@ package uchinchi_oy;
 public class poligon_2 {
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("cursed with knowledge");
+        System.out.println("\n\n cursed with knowledge");
     }
 }
